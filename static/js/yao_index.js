f26e2f0f2b1d22b27601a93a75ea8c2d8e7adf5d
chazzhuang(黄长志) <chazzhuang@tencent.com>
2018-07-01 16:29:39 +0800

    判断 是否选择上传文件，上传文件是否是xls

--- static/js/yao_index.js
+++ static/js/yao_index.js
@@ -6,6 +6,23 @@ $(document).ready(function () {
         $("#loading").show();
 
         var files = $("#files_upload").get(0).files;
+
+        if (files.length == 0) {
+            M.toast({ html: "请先选择上传文件", classes: "red" });
+
+            $("#loading").hide();
+            return;
+        }
+
+        for (var i = 0; i < files.length; i++) {
+            if (!files[i].name.toLowerCase().endsWith(".xls")){
+                M.toast({ html: "请仅选择上传xls文件", classes: "red" });
+
+                $("#loading").hide();
+                return;
+            }
+        }
+
         var formData = new FormData();
         for (var i = 0; i < files.length; i++) {
             formData.append("file", files[i]);
@@ -24,7 +41,7 @@ $(document).ready(function () {
                 processData: false
 
             }).success(function (html) {
-            $("body").html(html);
-        })
+                $("body").html(html);
+            })
     });
 });
